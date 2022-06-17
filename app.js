76fb0515375d505afb12f080052ad0e1e97a5305
Ariyan Rehman <ariyangd@outlook.com>
2022-06-17 23:34:05 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const request = require("request");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+
+    var firstName = req.body.fName;
+    var lastName = req.body.lName;
+    var email = req.body.email;
+    console.log(firstName, lastName, email)
+
+});
+
+app.listen(3000, function () {
+    console.log("Server started on port 3000");
+});
