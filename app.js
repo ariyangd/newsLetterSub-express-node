7179fd812e958cf57eb4c3590e480ff7b11c34ba
Ariyan Rehman <ariyangd@outlook.com>
2022-06-21 15:00:18 +0600

    2nd commit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const request = require("request");
 const bodyParser = require("body-parser");
+const http = require("https");
+
 
 const app = express();
 
@@ -15,13 +17,47 @@ app.get("/", (req, res) => {
 
 app.post("/", function (req, res) {
 
-    var firstName = req.body.fName;
-    var lastName = req.body.lName;
-    var email = req.body.email;
-    console.log(firstName, lastName, email)
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
 
+
+    const data = {
+        members: [{
+            email_address: email,
+            status: "Subscribed",
+            marge_fields: {
+                FNAME: firstName,
+                LNAME: lastName,
+
+            }
+        }]
+    };
+
+    const jasonData = JSON.stringify(data);
+    const url = "https://us14.api.mailchimp.com/3.0/lists/558b38c676";
+    const options = {
+        method: "POST",
+        auth: "ariyan:0246012c77e239a99775ac2fe0d9c323-us14"
+    };
+
+
+    const request = http.request(url, options, (response) => {
+        response.on("data", function (data) {
+            console.log(JSON.parse(data));
+        })
+    })
+    request.write(jasonData);
+    request.end();
 });
+
 
 app.listen(3000, function () {
     console.log("Server started on port 3000");
 });
+
+//api
+//0246012c77e239a99775ac2fe0d9c323-us14
+
+//list ID
+//558b38c676
